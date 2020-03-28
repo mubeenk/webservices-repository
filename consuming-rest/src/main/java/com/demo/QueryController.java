a77e059eb8cbd3f8b3c775f0d1eeb64aa9472b17
mubeenk <mkhantx@gmail.com>
2020-03-28 12:41:59 -0500

    updated for OpenAPI

--- consuming-rest/src/main/java/com/demo/QueryController.java
+++ consuming-rest/src/main/java/com/demo/QueryController.java
@@ -1,14 +1,26 @@
 package com.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class QueryController {
 
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@GetMapping("/rest")
 	public String getResult() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/quote")
+	public Quote getQuote() {
+		Quote quote = restTemplate.getForObject(
+				"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+		
+		return quote;
 	}
 }
